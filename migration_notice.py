c4d45d07511fde148169c326693d19976657b1df
Alex Osborne <ato@meshy.org>
2018-06-30 17:46:19 +0900

    Update migration_notice.py

--- migration_notice.py
+++ migration_notice.py
@@ -1,29 +1,60 @@
 #!/usr/bin/env python3
-import sys, os, requests
+import sys, os, requests, re
+from glob import glob
+from urllib.parse import quote
 
-page_id = int(sys.argv[1])
-github_name = sys.argv[2]
-auth = (os.environ['API_USER'], os.environ['API_TOKEN'])
+if 'API_USER' in os.environ:
+    api_auth = (os.environ['API_USER'], os.environ['API_TOKEN'])
+else:
+    print('API_USER and API_TOKEN not set, doing dry run.')
+    api_auth = None # dry run
 
-api_url = "https://webarchive.jira.com/wiki/rest/api/content/" + str(page_id)
-github_url = "https://github.com/internetarchive/heritrix3/wiki/" + github_name
+def migrate(page_id):
+    api_url = "https://webarchive.jira.com/wiki/rest/api/content/" + str(page_id)
+    old = requests.get(api_url).json()
 
-text = "<p>This page has moved to <a href='" + github_url + "'>" + github_name + "</a> on the Github wiki.</p>"
+    github_name = old['title']
+    #github_url = "https://github.com/internetarchive/heritrix3/wiki/" + quote(github_name)
+    github_url = "https://github.com/ato/heritrix-wiki-conversion/wiki/" + quote(github_name)
+    text = "<p>This page has moved to <a href='" + github_url + "'>" + github_name + "</a> on the Github wiki.</p>"
 
-old = requests.get(api_url).json()
-
-update = {
-    "type": "page",
-    "title": old['title'],
-    "body": {"storage": {"value": text, "representation": "storage"}},
-    "version": {
-        "number": old['version']['number'] + 1,
-        "minorEdit": True,
-        "message": "Migrated to Github wiki"
+    update = {
+        "type": "page",
+        "title": old['title'],
+        "body": {"storage": {"value": text, "representation": "storage"}},
+        "version": {
+            "number": old['version']['number'] + 1,
+            "minorEdit": True,
+            "message": "Migrated to Github wiki"
+        }
     }
-}
 
-response = requests.put(api_url, json=update, auth=auth)
+    # check that the github URL is correct
+    if requests.head(github_url).status_code != 200:
+        print(page_id, github_url, 'does not exist')
+        return
 
-print(response.status_code)
-print(response.text)
+    if api_auth is None:
+        print(page_id, 'OK. But dry run.')
+        return
+
+    response = requests.put(api_url, json=update, auth=api_auth)
+    if response.status_code != 200:
+        print('Failed:', page_id, response.status_code, response.text)
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage:', sys.argv[0], 'path-to-confluence-html-export')
+        sys.exit(1)
+
+    export_path = sys.argv[1]
+    for file in glob(export_path + '/*.html'):
+        filename = os.path.basename(file)
+        if filename == 'index.html':
+            continue
+        if '_' in filename: # e.g. Avoiding-Too-Much-Dynamic-Content_5735747.html
+            filename = filename.split('_')[-1]
+        page_id = filename.split('.')[0]
+        migrate(page_id)
+
+if __name__ == '__main__': main()
