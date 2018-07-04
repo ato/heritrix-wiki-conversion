797537606ed091554d582489bdde293a197b7273
Alex Osborne <aosborne@nla.gov.au>
2018-07-04 15:32:24 +0900

    Copyright notice

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Heritrix'
-copyright = '2018, Internet Archive'
-author = 'Internet Archive'
+copyright = '2018 Internet Archive and Heritrix contributors'
+author = 'Internet Archive nad Heritrix contributors'
 
 # The short X.Y version
 version = ''
@@ -75,8 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
